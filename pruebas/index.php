1fae34e95b30ed8db63ca1a0fa2ca64e8bf75b82
Latifi <Latifiyates@gmail.com>
2024-10-17 11:01:39 +0200

    php

--- pruebas/index.php
+++ pruebas/index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prueba Ficheros</title>
 </head>
+
 <body>
     <form action="subir.php" method="post" enctype="multipart/form-data">
-    <input type="text" name="nombre">
-    <input type="file" name="fichero">
-    <button>Subir</button>
+        <input type="text" name="nombre">
+        <input type="file" name="fichero">
+        <button>Subir</button>
     </form>
 </body>
+
 </html>
